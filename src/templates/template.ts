00ce83fd197d002c272abc3e45b93d181d645153
Einer Bravo <einerbravo@gmail.com>
2023-01-12 12:43:59 -0500

    minor changes

--- src/templates/template.ts
+++ src/templates/template.ts
@@ -7,11 +7,12 @@ export const Template = [
     variable: 1,
     enabled: true,
   },
-  /* {
+  {
     id: 2,
     name: "test_como_te_ha_ido",
     template:
-      "Hola, te damos la bienvenida a Bolsiyo. Estamos super emocionados de empezar el camino de digitalización contigo. Queremos acompañarte y por esto hemos te hemos asignado un asistente especializado que te acompañará en el proceso. Quieres concoerlo?",
-
-  }, */
+      "Hola, te damos la bienvenida a Bolsiyo.\nEstamos super emocionados de empezar el camino de digitalización contigo. Queremos acompañarte y por esto hemos te hemos asignado un asistente especializado que te acompañará en el proceso. Quieres concoerlo?",
+    variable: 0,
+    enabled: true,
+  },
 ];
